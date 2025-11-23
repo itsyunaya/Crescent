93abbbaf1a35b75443deb5032fb7b9cd428c8e0b
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2025-11-23 22:19:07 +0100

    logic still not implemented but atleast the function doesnt crash the game anymore

--- src/main/java/com/itsyunaya/crescent/mixin/ScreenHandlerMixin.java
+++ src/main/java/com/itsyunaya/crescent/mixin/ScreenHandlerMixin.java
@@ -3,7 +3,6 @@ package com.itsyunaya.crescent.mixin;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
@@ -16,19 +15,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static com.itsyunaya.crescent.util.Utils.mc;
 
 @Mixin(ScreenHandler.class)
-public class ScreenHandlerMixin {
+public abstract class ScreenHandlerMixin {
 
-    @Inject(method = "onSlotClick", at = @At("HEAD"))
+    @Inject(method = "onSlotClick", at = @At("HEAD"), cancellable = true)
     public void crescent$ctrlClickTakeItem(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
-        Slot focusedSlot = ((HandledScreenAccessor) player.getInventory()).getFocusedSlot();
+        if (mc.currentScreen instanceof InventoryScreen inv) {
+            Slot focusedSlot = ((HandledScreenAccessor) inv).getFocusedSlot();
 
-        if (mc.currentScreen instanceof InventoryScreen
-                && focusedSlot != null
-                && focusedSlot.hasStack()
-                && button == 0
-                && (InputUtil.isKeyPressed(mc.getWindow(), GLFW.GLFW_KEY_LEFT_CONTROL) || InputUtil.isKeyPressed(mc.getWindow(), GLFW.GLFW_KEY_RIGHT_CONTROL))
-        ) {
+            if (focusedSlot != null
+                    && focusedSlot.hasStack()
+                    && button == 0
+                    && (InputUtil.isKeyPressed(mc.getWindow(), GLFW.GLFW_KEY_LEFT_CONTROL) || InputUtil.isKeyPressed(mc.getWindow(), GLFW.GLFW_KEY_RIGHT_CONTROL))
+            ) {
+                ci.cancel();
 
+            }
         }
     }
 }
